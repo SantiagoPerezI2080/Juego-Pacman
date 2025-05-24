6ee35a5a680108e3ab992b94199ffacb29ade87b
Bryan Gaviria <bryan.gaviria.c@uniautonoma.edu.co>
2025-05-24 15:34:52 -0500

    Solucion al problema de colisiones

--- models/game_model.py
+++ models/game_model.py
@@ -31,36 +31,38 @@ class GameModel:
             if cell == 0 and (x, y) != self.pacman.position
         ]
         # Elige 4 posiciones aleatorias distintas
-        self.ghosts = [Ghost(pos) for pos in random.sample(libres, 3)]
-
-
-
-    def update(self) -> None:
-        # 1) Mover Pac‑Man
-        self.pacman.move(self.walls)
-
-        # 2) Comprobar colisión con fantasmas
-        if self.check_collision_with_ghosts():
-            self.game_over = True
-
-        # 3) Recolectar comida
-        if self.pacman.position in self.food:
-            self.food.remove(self.pacman.position)
-            self.score_display.score += 10
-
-        # 4) Mover fantasmas
-        for ghost in self.ghosts:
-            ghost.move(self.walls)
-
-
-       #5) Comprobar colisión con fantasmas
+        self.ghosts = [Ghost(pos) for pos in random.sample(libres, 1)]
+    
     def check_collision_with_ghosts(self) -> bool:
         for ghost in self.ghosts:
             if ghost.position == self.pacman.position:
                 return True
         return False
 
+    def update(self) -> None:
+        # Guarda posiciones anteriores
+        pacman_prev = self.pacman.position
+        ghosts_prev = [ghost.position for ghost in self.ghosts]
 
-        
-        
-        
+        # 1) Mover Pac‑Man
+        self.pacman.move(self.walls)
+
+        # 2) Mover fantasmas
+        for ghost in self.ghosts:
+            ghost.move(self.walls)
+
+        # 3) Comprobar colisión directa
+        if self.check_collision_with_ghosts():
+            self.game_over = True
+            return
+
+        # 4) Comprobar colisión cruzada (swap)
+        for ghost, ghost_prev in zip(self.ghosts, ghosts_prev):
+            if self.pacman.position == ghost_prev and pacman_prev == ghost.position:
+                self.game_over = True
+                return
+
+        # 5) Recolectar comida
+        if self.pacman.position in self.food:
+            self.food.remove(self.pacman.position)
+            self.score_display.score += 10
